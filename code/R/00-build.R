9bd8987a039d888f494b690a7d52aef8323c2140
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2019-08-06 13:00:01 -0500

    remove old stuff from build file

--- code/R/00-build.R
+++ code/R/00-build.R
@@ -3,7 +3,6 @@
 #   R master file: controls replication of all R things
 # ----------------------------------------------------
 
-
 # --- build it -----------------------
 
 source(here::here("code", "R", "01-setup.R"), echo = TRUE)
@@ -18,15 +17,4 @@ source(here("code", "R", "04-MCMC.R"), echo = TRUE)
 source(here::here("code", "R", "05-viz.R"), echo = TRUE)
 # save.image(file = here("data", "rmd-rdata", "05-viz.Rdata"))
 
-
-# source("R/mcmc-viz.R",, echo = TRUE)
-
 beepr::beep(2)
-
-# source("R/04-define-model.R", echo = TRUE)
-
-# source("R/05-run-model.R", echo = TRUE)
-
-# source("R/06-OCD.R")
-
-# source("R/07-ocd-viz.R")
