9eb23e309b33211c72b4ff65bcd9c468632968c7
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2019-08-06 14:20:57 -0500

    fix fonts in .tex, graphics

--- writing-rmd/assets/setup-Rmarkdown.R
+++ writing-rmd/assets/setup-Rmarkdown.R
@@ -18,15 +18,16 @@ knitr::opts_chunk$set(
   eval = TRUE, echo = FALSE, include = FALSE, 
   warning = FALSE, message = FALSE,
   cache = TRUE, collapse = TRUE,
-  fig.path = "figs/",
-  # cache path = ...,
+  fig.path = here("writing-rmd", "paper-figs/"),
+  cache.path = here("writing-rmd", "paper-cache/"),
   # fig.retina = 2, dpi = 100,
   dev = "cairo_pdf", fig.align = "center"
 )
 
 # graphics theme
+update_geom_defaults("text", list(family = "Fira Sans"))
 theme_set(
-  ggthemes::theme_base(base_family = "Source Sans Pro", base_size = 14) + 
+  ggthemes::theme_base(base_family = "Fira Sans", base_size = 14) + 
   theme(plot.background = element_blank(), 
         axis.ticks = element_line(lineend = "square"), 
         axis.ticks.length = unit(0.25, "lines"))
