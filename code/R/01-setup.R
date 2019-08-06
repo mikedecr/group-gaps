e84dac4c7477e74ce459e3fbf8d9cbcbf5f73983
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2019-08-06 13:26:30 -0500

    graphics theme uses Fira Sans

--- code/R/01-setup.R
+++ code/R/01-setup.R
@@ -41,10 +41,14 @@ dir.create(here("tex/appendix"))
 
 # --- graphics settings -----------------------
 
-#  custom ggtheme, import from Github
-source("https://raw.githubusercontent.com/mikedecr/custom-R/master/theme-mgd/theme-mgd.R")
-theme_set(theme_mgd()) # set default theme
-# theme_set(theme_bw())
+# graphics theme
+update_geom_defaults("text", list(family = "Fira Sans"))
+theme_set(
+  ggthemes::theme_base(base_family = "Fira Sans", base_size = 14) + 
+  theme(plot.background = element_blank(), 
+        axis.ticks = element_line(lineend = "square"), 
+        axis.ticks.length = unit(0.25, "lines"))
+)
 
 dblue <- "#0072B2"
 rred <- "#D55E00"
