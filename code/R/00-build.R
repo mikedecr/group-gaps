b3554adf342357887776cfa4540df4a27a4cafca
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2019-08-13 12:21:40 -0500

    rdata dump filepaths

--- code/R/00-build.R
+++ code/R/00-build.R
@@ -10,11 +10,11 @@ source(here::here("code", "R", "01-setup.R"), echo = TRUE)
 source(here::here("code", "R", "02-recode-anes.R"), echo = TRUE)
 
 source(here::here("code", "R", "03-descriptives.R"), echo = TRUE)
-# save.image(file = here("data", "rmd-rdata", "03-descriptives.Rdata"))
+save.image(file = here("data", "rmd-rdata", "03-descriptives.Rdata"))
 
 source(here::here("code", "R", "04-MCMC.R"), echo = TRUE)
 
 source(here::here("code", "R", "05-viz.R"), echo = TRUE)
-# save.image(file = here("data", "rmd-rdata", "05-viz.Rdata"))
+save.image(file = here("data", "rmd-rdata", "05-viz.Rdata"))
 
 beepr::beep(2)
