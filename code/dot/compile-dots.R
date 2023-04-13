7ba29bd1c1d9d157195cc0ec66b0f1bed41ffb58
Michael DeCrescenzo <michaeldecrescenzo@Michaels-MacBook-Pro.local>
2023-04-12 23:53:18 -0500

    safer dot compilation file

--- code/dot/compile-dots.R
+++ code/dot/compile-dots.R
@@ -1,12 +1,14 @@
 # ---- compile dot files into PDF -----------------------
 
 # read dot
-dots <- list.files(here("code", "dot"), pattern = "[.]dot")
-stems <- str_replace(dots, "[.]dot", "")
+dots <- list.files(here::here("code", "dot"), pattern = "[.]dot")
+stems <- stringr::str_replace(dots, "[.]dot", "")
+
+system('mkdir writing-rmd/assets/dot-img')
 
 # compile to PDF
 # set font if needed
 lapply(stems, function(stem) 
-       str_glue("dot -Tpdf -Nfontname='Linux Libertine' code/dot/{stem}.dot > writing-rmd/assets/dot-img/{stem}.pdf") %>%
-         as.character() %>%
+       stringr::str_glue("dot -Tpdf -Nfontname='Linux Libertine' code/dot/{stem}.dot > writing-rmd/assets/dot-img/{stem}.pdf") |>
+         as.character() |>
          system())
