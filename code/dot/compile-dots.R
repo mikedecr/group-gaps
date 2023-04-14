1818c02934d94e43eecdaac9a067c31a117c5a4f
Michael DeCrescenzo <michaeldecrescenzo@Michaels-MacBook-Pro.local>
2023-04-13 22:34:28 -0500

    fix .dot output dir

--- code/dot/compile-dots.R
+++ code/dot/compile-dots.R
@@ -4,11 +4,15 @@
 dots <- list.files(here::here("code", "dot"), pattern = "[.]dot")
 stems <- stringr::str_replace(dots, "[.]dot", "")
 
-system('mkdir writing-rmd/assets/dot-img')
+system("mkdir paper/assets/dot-img")
 
 # compile to PDF
 # set font if needed
-lapply(stems, function(stem) 
-       stringr::str_glue("dot -Tpdf -Nfontname='Linux Libertine' code/dot/{stem}.dot > writing-rmd/assets/dot-img/{stem}.pdf") |>
-         as.character() |>
-         system())
+lapply(
+    stems,
+    \(stem) {
+       stringr::str_glue("dot -Tpdf -Nfontname='Linux Libertine' code/dot/{stem}.dot > paper/assets/dot-img/{stem}.pdf") |>
+       as.character() |>
+       system()
+    }
+)
