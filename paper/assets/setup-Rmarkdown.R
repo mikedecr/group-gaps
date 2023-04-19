a5134cff22305456e4f36bec59b607ee82959b04
Michael DeCrescenzo <michaeldecrescenzo@Michaels-MacBook-Pro.local>
2023-04-18 21:30:02 -0500

    [knit] fig and cache paths

--- paper/assets/setup-Rmarkdown.R
+++ paper/assets/setup-Rmarkdown.R
@@ -6,8 +6,8 @@ knitr::opts_chunk$set(
   eval = TRUE, echo = FALSE, include = FALSE,
   warning = FALSE, message = FALSE,
   cache = TRUE, collapse = TRUE,
-  fig.path = here("paper", "figs/"),
-  cache.path = here("paper", "cache/"),
+  fig.path = here("paper", "chunk-figs/"),
+  cache.path = here("paper", "chunk-cache/"),
   # fig.retina = 2, dpi = 100,
   dev = "cairo_pdf", fig.align = "center"
 )
