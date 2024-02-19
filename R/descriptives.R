c9dabb1e7cbe96746d8c4e4a1e6a32477339a719
mikedecr <mgdecrescenzo@gmail.com>
2024-02-19 14:53:15 -0600

    slight descriptive update

--- R/descriptives.R
+++ R/descriptives.R
@@ -5,10 +5,13 @@
 
 library("conflicted")
 library("dplyr")
+library("tidyr")
 library("here")
 library("tibble")
 library("purrr")
 library("ggplot2")
+
+conflicts_prefer(dplyr::filter)
 
 box::use(intervals = ./R/lib/confints)
 box::use(plt = ./R/lib/graphics_theme)
@@ -19,22 +22,22 @@ box::use(./R/lib/maps[...])
 #   data
 # ----------------------------------------------------
 
-anes <- here("data", "clean", "cleaned-anes-cdf.RDS") |>
-    readRDS() |>
+anes <- here("data", "clean", "anes_cdf.pq") |>
+    arrow::read_parquet() |>
     as_tibble()
 
 
-with(anes, table(pid_init, exclude = NULL))
 
 
 # ----------------------------------------------------
 #   Trends in Party ID
 # ----------------------------------------------------
 
+with(anes, table(pid_init, exclude = NULL))
+
 # calculate proportion in each party ID and plot
 
-
-plot_pid_tab <- anes |>
+agg_party_gender <- anes |>
     group_by(pid_lean, gender, cycle) |>
     count(wt = wt) |>
     group_by(cycle, gender) |>
@@ -47,17 +50,16 @@ plot_pid_tab <- anes |>
         upper = map_dbl(ci, pfx$item, "upper")
     )
 
-ggplot(dplyr::filter(plot_pid_tab, pid_lean %in% c("Dem", "Rep"))) +
+ggplot(dplyr::filter(agg_party_gender, pid_lean %in% c("Dem", "Rep"))) +
     aes(x = cycle,  y = prop) +
     facet_wrap(~ gender,
                labeller = as_labeller(map_gender_code_name)) +
     geom_hline(yintercept = 0.5) +
     geom_ribbon(aes(ymin = lower, ymax = upper, fill = pid_lean),
-                alpha = 0.3, show.legend = FALSE) +
-    geom_line(aes(color = pid_lean),
-              show.legend = FALSE) +
+                alpha = 0.3) +
+    geom_line(aes(color = pid_lean)) +
     geom_point(aes(shape = pid_lean, color = pid_lean),
-               fill = "white", show.legend = FALSE) +
+               fill = "white") +
     annotate("text", x = 1980, y = 0.63, size = 3.5, label = "Democrats") +
     annotate("text", x = 1980, y = 0.25, size = 3.5, label = "Republicans") +
     coord_cartesian(ylim = c(.20, .75)) +
@@ -69,7 +71,9 @@ ggplot(dplyr::filter(plot_pid_tab, pid_lean %in% c("Dem", "Rep"))) +
     scale_x_continuous(breaks = seq(1952, 2016, 8)) +
     scale_y_continuous(breaks = seq(0, 1, .10), labels = scales::percent) +
     theme(axis.text.x = element_text(angle = 45, vjust = 0.75),
-          panel.grid.minor = element_blank())
+          panel.grid.minor = element_blank(),
+          legend.position = "none"
+    )
 
 
 # ----------------------------------------------------
@@ -78,24 +82,24 @@ ggplot(dplyr::filter(plot_pid_tab, pid_lean %in% c("Dem", "Rep"))) +
 
 # filter: must be asked about voting
 
-plot_turnout_gender <- anes |>
-    dplyr::filter(!is.na(voted)) |>
-    group_by(gender, cycle, voted) |>
-    summarize(n = sum(wt)) |>
+agg_turnout_gender = anes |>
+    filter(is.na(voted) == FALSE) |>
     group_by(gender, cycle) |>
-    mutate(n_cycle = sum(n)) |>
-    ungroup() |>
-    mutate(
-         prop = (n / n_cycle),
-         ci = map2(n, n_cycle, intervals$prop_ci),
-         lower = map_dbl(ci, pfx$item, 'lower'),
-         upper = map_dbl(ci, pfx$item, 'upper')
+    summarize(
+        y = sum(wt[voted == 1]),
+        n = sum(wt),
+        prop = weighted.mean(voted, wt),
+        .groups = "drop"
     ) |>
-    select(-ci) |>
-    print()
+    mutate(
+        ci = map2(y, n, intervals$prop_ci),
+        lower = map_dbl(ci, pfx$item, "lower"),
+        upper = map_dbl(ci, pfx$item, "upper")
+    ) |>
+    select(-ci)
 
 
-ggplot(dplyr::filter(plot_turnout_gender, voted == 1)) +
+ggplot(agg_turnout_gender) +
     aes(x = cycle, y = prop) +
     geom_ribbon(aes(ymin = lower, ymax = upper, fill = gender),
                 alpha = 0.3,
@@ -133,8 +137,7 @@ ggplot(dplyr::filter(plot_turnout_gender, voted == 1)) +
 #   THE GENDER GAP
 # ----------------------------------------------------
 
-
-plot_vote_gender <- anes |> 
+agg_vote_gender <- anes |> 
     dplyr::filter(vote_choice %in% c("Dem Cand", "Rep Cand")) |>
     group_by(cycle, gender, vote_choice) |>
     summarize(n = sum(wt), .groups = "drop") |>
@@ -147,36 +150,34 @@ plot_vote_gender <- anes |>
         lower = map_dbl(ci, pfx$item, 'lower'),
         upper = map_dbl(ci, pfx$item, 'upper')
     ) |>
-    ungroup() |>
     dplyr::filter(vote_choice == "Dem Cand") |>
     print()
 
-ggplot(plot_vote_gender) +
+ggplot(agg_vote_gender) +
     aes(x = cycle, y = dem_share, 
          ymin = lower, ymax = upper, 
          color = gender, fill = gender) +
-    geom_hline(yintercept = 0.5, color = 'gray50') +
-    geom_ribbon(aes(color = NULL), 
-              alpha = 0.3,
-              show.legend = FALSE) +
-    geom_line(show.legend = FALSE) +
-    geom_point(aes(shape = gender),
-             size = 2,
-             fill = "white",
-             show.legend = FALSE) +
+    geom_hline(yintercept = 0.5, color = "gray50") +
+    geom_ribbon(aes(color = NULL), alpha = 0.3) +
+    geom_line() +
+    geom_point(aes(shape = gender), size = 2, fill = "white") +
     annotate("text", size = 3.5, x = 1986, y = 0.68, label = "Women") +
     annotate("text", size = 3.5, x = 1991, y = 0.37, label = "Men") +
-    labs(x = "Election Cycle", 
-       y = "Democratic Share of\nTwo-Party Vote", 
-       color = NULL, fill = NULL) +
+    labs(x = "Election Cycle",
+         y = "Democratic Share of Two-Party Vote",
+         color = NULL,
+         fill = NULL) +
     plt$scale_color_gender() +
     plt$scale_fill_gender() +
     scale_shape_manual(values = c(22, 16)) +
     scale_x_continuous(breaks = seq(1952, 2016, 8),
                        minor = seq(1952, 2016, 4)) +
     scale_y_continuous(labels = scales::percent) +
-    theme(axis.text.x = element_text(angle = 45, vjust=0.75),
-        panel.grid.minor = element_blank())
+    theme(
+        axis.text.x = element_text(angle = 45, vjust=0.75),
+        panel.grid.minor = element_blank(),
+        legend.position = "none"
+    )
 
 
 # --- gap as difference -----------------------
@@ -191,7 +192,7 @@ gap_frame = anes |>
         n_cycle = sum(wt),
         .groups = "drop"
     ) |>
-    tidyr::pivot_wider(names_from = "gender", values_from = dem_share:n_cycle) |>
+    pivot_wider(names_from = "gender", values_from = dem_share:n_cycle) |>
     group_by_all() |>
     mutate(
         ci_diff = pmap(list(n_W, n_cycle_W, n_M, n_cycle_M),
@@ -204,8 +205,7 @@ gap_frame = anes |>
         upper = map_dbl(ci_diff, pfx$item, 'upper')
     )
 
-fig_vote_diff <- 
-  ggplot(gap_frame, aes(x = cycle, y = estimate)) + 
+ggplot(gap_frame, aes(x = cycle, y = estimate)) + 
     geom_hline(yintercept = 0, color = 'gray50') +
     geom_ribbon(aes(ymin = lower, ymax = upper),
                 alpha = 0.3) + 
@@ -238,7 +238,7 @@ fig_vote_diff <-
 # ----------------------------------------------------
 
 # data from https://uselectionatlas.org/RESULTS/
-leip <- read_csv(here("data/leip-national-pop-vote-data.csv")) |>
+leip <- readr::read_csv(here("data/leip-national-pop-vote-data.csv")) |>
   mutate(dem_vote_share = dem_raw_votes / (dem_raw_votes + rep_raw_votes)) |>
   left_join(gap_frame) |>
   print()
@@ -250,9 +250,7 @@ leip <- read_csv(here("data/leip-national-pop-vote-data.csv")) |>
  #  select(cycle, gender_gap, dem_vote_share) |>
  #  print
 
-
-
-ggplot(data = leip, aes(x = gender_gap, dem_vote_share)) +
+ggplot(data = leip, aes(x = estimate, dem_vote_share)) +
   geom_hline(yintercept = 0.5) +
   geom_vline(xintercept = 0) +
   geom_smooth(method = "lm", 
@@ -265,8 +263,11 @@ ggplot(data = leip, aes(x = gender_gap, dem_vote_share)) +
   labs(x = "Gender Gap (%)", y = "Democratic Share of Two-Party Vote (%)")
 
 
-ggsave("tex/graphics/gap-vote-scatter.pdf", 
-       height = 4, width = 5, device = cairo_pdf)
+# ggsave("tex/graphics/gap-vote-scatter.pdf", 
+#        height = 4, width = 5, device = cairo_pdf)
+
+
+
 
 
 
@@ -361,8 +362,8 @@ ggplot(data = exits, aes(x = gender_gap, y = dem_share)) +
        y = "Democratic Share of\nTwo-Party Vote (%)")
 
 
-ggsave("tex/graphics/exit-poll-scatter.pdf", 
-       height = 3, width = 6, device = cairo_pdf)
+# ggsave("tex/graphics/exit-poll-scatter.pdf", 
+#        height = 3, width = 6, device = cairo_pdf)
 
 
 
@@ -519,8 +520,8 @@ ggplot(toplines, aes(x = gender_gap, y = dem_vote_share)) +
   theme(panel.grid = element_blank()) +
   NULL
 
-ggsave(here("tex/graphics/topline-3-panel.pdf"), 
-       height = 3, width = 7, device = cairo_pdf)
+# ggsave(here("tex/graphics/topline-3-panel.pdf"), 
+#        height = 3, width = 7, device = cairo_pdf)
 
 
 
